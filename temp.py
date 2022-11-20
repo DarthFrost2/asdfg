23170d5c0a790700e2a2d0da5ebf0a1cda6033ee
DarthFrost2 <91642106+DarthFrost2@users.noreply.github.com>
2022-11-20 09:47:31 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,3 +1,10 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score 
+from sklearn import datasets
+from sklearn import svm
 import pickle
 import streamlit as st
 from streamlit_option_menu import option_menu
